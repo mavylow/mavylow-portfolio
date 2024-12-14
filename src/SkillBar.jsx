9f8997de7ed7e5d18b47aa16298e43d8d4779469
mavylow <majsofia2005@gmail.com>
2024-12-14 17:24:50 +0300

    edit skillbar

--- src/SkillBar.jsx
+++ src/SkillBar.jsx
@@ -9,31 +9,31 @@ export default function SkillBar({
   return (
     <div className="flex flex-col w-auto relative ">
       <span
-        className={`flex justify-center text-[9px]  w-auto px-[0.7vw] pt-0.5 rounded-[6px] z-20 h-4 laptop:text-[1.1vw] laptop:h-[1.7vw] ${
+        className={`flex justify-center text-[10px] py-1  w-auto px-2 pt-0.5 rounded-[6px] z-20 h-4 laptop:text-[1.1vw] laptop:h-[2.2vw] laptop:px-3 laptop:pt-1  laptop:rounded-[1vw] laptop:min-w-16 ${
           theme === "night" ? "bg-night-bg" : "bg-day-bg"
         }`}
       >
         {skillName}
       </span>
       <div
-        className={`px-2 rounded-[6px] absolute  h-[18px] ${
+        className={`px-2 rounded-[6px] absolute  h-[19px] ${
           theme === "night"
             ? "bg-night-progress"
             : "bg-day-progress"
-        }  z-10 laptop:h-[1.95vw] rounded-[8px]`}
+        }  z-10 laptop:h-[2.5vw] laptop:rounded-[1vw]`}
         style={{ width: `${percentage}%` }}
       ></div>
       <div
-        className={`px-2 rounded-[6px] absolute  h-[18px]  ${
+        className={`px-2 rounded-[6px] absolute  h-[19px]  ${
           theme === "night" ? "bg-night-bg" : "bg-day-bg"
-        } z-0 w-full laptop:h-[1.9vw] laptop:rounded-[8px]`}
+        } z-0 w-full laptop:h-[2.5vw] laptop:rounded-[1vw]`}
       ></div>
       <div
-        className={`px-2 rounded-[6px] absolute z-30 h-[18px]  w-full border laptop:rounded-[8px] ${
+        className={`px-2 rounded-[6px] absolute z-30 h-[19px]  w-full border  ${
           theme === "night"
             ? "border-night-progress"
             : "border-day-progress"
-        } laptop:h-[1.95vw] laptop:rounded-[8px] laptop:border-2`}
+        } laptop:h-[2.5vw] laptop:rounded-[1vw] laptop:border-[1.5px]`}
       ></div>
     </div>
   );
